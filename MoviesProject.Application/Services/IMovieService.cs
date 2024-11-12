8bd52b8ce1d38066146a3c3ef4b30b98884a03b2
michal2612 <32363382+michal2612@users.noreply.github.com>
2024-11-12 08:18:59 +0000

    Update IMovieService.cs

--- MoviesProject.Application/Services/IMovieService.cs
+++ MoviesProject.Application/Services/IMovieService.cs
@@ -1,20 +1,22 @@
-﻿using MoviesProject.Application.DTO;
+using MoviesProject.Application.DTO;
 
 namespace MoviesProject.Application.Services;
 
 /// <summary>
-/// 
+/// Defines the contract for movie-related operations.
 /// </summary>
 public interface IMovieService
 {
     /// <summary>
-    /// 
+    /// Searches for movies based on title, genre, and actor with specified sorting, limit, and pagination options.
     /// </summary>
-    /// <param name="movieTitle"></param>
-    /// <param name="genre"></param>
-    /// <param name="limit"></param>
-    /// <param name="pageOffset"></param>
-    /// <returns></returns>
+    /// <param name="movieTitle">The title or partial title of the movie to search for.</param>
+    /// <param name="genre">The genre to filter movies by.</param>
+    /// <param name="actor">The actor to filter movies by.</param>
+    /// <param name="sortingMethod">The method to sort the search results.</param>
+    /// <param name="limit">The maximum number of results to return.</param>
+    /// <param name="pageOffset">The offset for pagination, indicating the starting point for results.</param>
+    /// <returns>A collection of movies matching the search criteria.</returns>
     public Task<IEnumerable<MovieDto>> FindMovie(string movieTitle, string genre, string actor,
         SortingMethod sortingMethod, int limit, int pageOffset);
 }
